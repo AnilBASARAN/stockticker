9020669bb54e63ad28539ddf113b82725c9b71fe
Anıl Başaran <nlbsrn@gmail.com>
2024-09-18 04:23:02 +0300

    made some style changes

--- src/Stocks.jsx
+++ src/Stocks.jsx
@@ -2,17 +2,26 @@ const Stocks=(props)=>{
 
     const {stock} = props;
     
-    const color = (stock.current_price - stock.previous_price) > 0 ? "text-green-400" : "text-red-400";
+    const color = (stock.current_price - stock.previous_price) > 0 ? "bg-green-500 text-white" : "bg-red-500 text-white";
       
       return(
-        <div className="flex flex-col bg-stone-100 m-12 p-2 shadow-md w-48 h-28">
-        <div></div>
-        <div className={color}>
+        <div className="flex justify-between items-center bg-stone-100 w-80 h-24 m-12 p-2 shadow-md">
+        <div className="leftSide">
+          <div>{stock.name}</div>
+        <div className="text-xl text-blue-400" >{stock.symbol}</div>
+        </div>
+    
+          <div className="rightSide" >
+    
+             <div className={`${color} rounded-full px-5`}>
       {(Math.round((stock.current_price - stock.previous_price) * 1000) / 1000 / stock.current_price).toFixed(3)}
     </div>
+            <div>$ {stock.current_price.toFixed(2)}</div>
+          
+          </div>
+       
     
-        <div>{stock.name}</div>
-        <div>ticker: {stock.symbol}</div>
+        
         
         </div>
         
